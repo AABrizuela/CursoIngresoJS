5215cc76bd09071a9848b698df1ae0d10138ed5c
Adrián <40859100+AABrizuela@users.noreply.github.com>
2018-07-09 22:53:03 -0300

    Practica ejercicios JS, entrada & salida.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
   
+	var elNombre;
+	var laLocalidad;
+
+	elNombre = document.getElementById('elNombre').value;
+	laLocalidad = document.getElementById('laLocalidad').value;
+
+	alert("Su nombre es " + elNombre + " y vive en " + laLocalidad)
 }
