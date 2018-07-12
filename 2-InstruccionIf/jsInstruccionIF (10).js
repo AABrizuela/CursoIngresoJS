e0958bfdcde376c48fec10ec12e1b7d22a9a1dbc
Adrián <40859100+AABrizuela@users.noreply.github.com>
2018-07-12 12:22:30 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,29 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero
+
+	numero = Math.floor(Math.random() * 10) + 1;
+
+	if(numero = 9 || numero = 10)
+	{
+	
+		alert("Excelente! Tu nota es un " + numero)
+
+	}
+
+	else
+	{
+		if(numero < 4 && numero > 9)
+		{
+			
+			alert("Tu nota es un " + numero + ", aprobó.")
+
+		}
+		else
+		{
+			alert("Tu nota es un " + numero + ". Vamos! La proxima se puede.")
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
