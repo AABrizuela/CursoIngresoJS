4d27e3a986cd7da4994a1c9f161dd28e0a714f96
Adrián <40859100+AABrizuela@users.noreply.github.com>
2018-07-13 12:13:10 -0300

    Ingreso Programacion Clase 4

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,14 +1,91 @@
 /*4.	Para el departamento de iluminación:
-Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
-A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
- ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+Tomando en cuenta que todas las lámparas están 
+en oferta al mismo precio de $35 pesos final.
+
+A.	Si compra 6 o más  lamparitas bajo consumo 
+tiene un descuento del 50%. 
+
+B.	Si compra 5  lamparitas bajo consumo 
+marca "ArgentinaLuz" se hace un descuento del 40 % 
+y si es de otra marca el descuento es del 30%.
+
+C.	Si compra 4  lamparitas bajo consumo marca 
+"ArgentinaLuz" o “FelipeLamparas” se hace un 
+descuento del 25 % y si es de otra marca el 
+descuento es del 20%.
+
+D.	Si compra 3  lamparitas bajo consumo marca 
+"ArgentinaLuz"  el descuento es del 15%, si es  
+“FelipeLamparas” se hace un descuento del 10 % y 
+si es de otra marca un 5%.
+
+E.	Si el importe final con descuento suma 
+más de $120  se debe sumar un 10% de ingresos 
+brutos en informar del impuesto con el siguiente 
+mensaje:
+
+ ”Usted pago X de IIBB.”, siendo X el impuesto 
+ que se pagó. 
 
  */
 function CalcularPrecio () 
 {
  	
+	var cantidad;
+	var marca;
+	var precio;
+	var precioSinDescuento;
+	var precioDescuento;
+
+	precio = 35;
+	cantidad = document.getElementById('Cantidad').value;
+	marca = document.getElementById('Marca').value;
+	precioSinDescuento = (precio * cantidad);
+
+	if (cantidad >= 6) 
+	{
+
+		precioDescuento = precioSinDescuento * 0.5;
+
+	}	
+		
+		else
+		{
+			if (marca == "ArgentinaLuz" && cantidad == 5) 
+			{
+
+			precioDescuento = precioSinDescuento * 0.4;
+
+			}
+			else
+			{
+		
+				if (marca != "ArgentinaLuz" && cantidad == 5)
+				{
+
+					precioDescuento = precioSinDescuento * 0.3;
+
+				}
+				else
+				{
+
+					if (marca == "ArgentinaLuz" || marca == "FelipeLamparas" && cantidad == 4) 
+					{
+
+						precioDescuento = 
+
+					}
+
+				}
+
+			}
+
+			
+
+		}
+
+	
+
+	document.getElementById('precioDescuento').value = precioDescuento;
+
 }
