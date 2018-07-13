d2a8830a0ee3ae311642d8b664fba1050092f948
Adrián <40859100+AABrizuela@users.noreply.github.com>
2018-07-13 12:12:39 -0300

    Ingreso Programacion Clase 4
    Switch ejercicios 1 al 5.

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,37 @@
+/*al seleccionar un mes informar. 
+si es Enero: "que comiences bien el año!!!." 
+si es Marzo: "a clases!!!." 
+si es Julio: "se vienen las vacaciones!!!." 
+si es Diciembre: "Felices fiesta!!!."*/
+
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
+	switch(mesDelAño)
+	{
+		case "Enero":
+		alert("Que comiences bien el año!!!")
+		break;
+		
+		case "Marzo":
+		alert("A clases!!!")
+		break;
 
+		case "Julio":
+		alert("Se vienen las vacaciones!!!")
+		break;
+
+		case "Diciembre":
+		alert("Felices fiestas!!!")
+		break;
+
+		default:
+		alert("Error.")
+		break;
+
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,67 @@
+/*al seleccionar un mes informar. 
+si estamos en Invierno: "Abrigate que hace frio." 
+si aún no llego el Invierno: "Falta para el invierno." 
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!." 
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.*/
+
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	alert (mesDelAño);
+
+	switch(mesDelAño)
+	{
+
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+		alert("Falta para el invierno.");
+		break;
+
+		case "Julio":
+		case "Agosto":
+		alert("Abrigate que hace frio.");
+		break;
+
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+		alert("Ya pasamos el frio, ahora el calor!!");
+		break;
+
+	}
+
 
 
 
 
 }//FIN DE LA FUNCIÓN
+
+/*
+
+Entrada y salida:
+
+1 al 10
+1 al 3 TP
+1 al 3 Recu/Parcial
+
+Instruccion If:
+
+1 al 10
+4 y 5 Recu/Parc
+Ferrete con If
+Ferrete If con Switch
+Ferrete Switch con If
+Ferrete Switch con Switch
+
+Instruccion Switch:
+
+1 al 6
+
+*/
