18d551d713e8586bd459800a89bfaba689c77a4a
Adrián <40859100+AABrizuela@users.noreply.github.com>
2018-07-13 12:14:08 -0300

    Ingreso Programacion Clase 4
    4 y 5 recu.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
 
+	var dia;
+
+	dia = prompt("Ingrese dia de la semana.");
+
+	switch(dia)
+	{
+
+		case "Domingo":
+				alert("Domingo")
+				break;
+
+		case 
+
+	}
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,69 @@
+/*(IF)Pedir dos números y mostrar el resultado: 
+Si son iguales los muestro concatenados. 
+Si el primero es mayor, los divido, 
+de lo contrario los sumo. 
+Si la suma es menor a 50 ,además de mostrar el resultado,
+muestro el mensaje: "la suma es xxx y es menor a 50"*/
+
 function mostrar()
 {
+
+	var	numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = prompt("Ingrese el primer numero.");
+	numeroDos = prompt("Ingrese el segundo numero.");
+
+	if (numeroUno == numeroDos) 
+
+	{
+
+		alert(numeroUno + numeroDos);
+
+	}
+
+	else
+	{
+
+		numeroUno = parseInt(numeroUno);
+		numeroDos = parseInt(numeroDos);
+
+		if (numeroUno > numeroDos) 
+		{
+
+			resultado = (numeroUno / numeroDos);
+			
+			alert(resultado);
+
+		}
+
+		else
+		{
+
+			if (numeroUno < numeroDos) 
+			{
+
+				resultado = (numeroUno + numeroDos);
+
+				if (resultado < 50) 
+				{
+
+					alert("La suma es " + resultado + " y es menor a 50");
+
+					else
+					{
+
+					alert(resultado);
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
 
 }
